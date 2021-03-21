7b8890110bfd2ea2461c30e1e89a8626d1c9c2cb
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2021-03-21 18:45:40 -0400

    Clean up db reference

--- src/ga/rugal/clojure/core/model/entity.clj
+++ src/ga/rugal/clojure/core/model/entity.clj
@@ -1,24 +1,23 @@
 (ns ga.rugal.clojure.core.model.entity
   "namespace for entity definition"
-  (:require [korma.core :as korma])
-  (:require [ga.rugal.clojure.core.model.db :refer :all]))
+  (:require [korma.core :refer [defentity table pk belongs-to has-many]]
+            [ga.rugal.clojure.core.model.db :refer :all]))
 
 (declare student course registration)
 
-(korma/defentity student
-                 (korma/table :school.student)
-                 (korma/pk :id)
-                 ;(entity-fields :rid :address :name :phone)
-                 (korma/has-many registration {:fk :id}))
+(defentity student
+           (table :school.student)
+           (pk :id)
+           (has-many registration {:fk :id}))
 
-(korma/defentity course
-                 (korma/table :school.course)
-                 (korma/pk :id)
-                 (korma/has-many registration {:fk :id}))
+(defentity course
+           (table :school.course)
+           (pk :id)
+           (has-many registration {:fk :id}))
 
-(korma/defentity registration
-                 (korma/table :school.registration)
-                 (korma/pk :id)
-                 (korma/belongs-to student)
-                 (korma/belongs-to course))
+(defentity registration
+           (table :school.registration)
+           (pk :id)
+           (belongs-to student)
+           (belongs-to course))
 
